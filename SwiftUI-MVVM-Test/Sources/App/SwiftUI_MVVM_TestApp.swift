ea6645643ddaf134a27b176a13652225c3a7b824
doyeonjeong <debby_@kakao.com>
2023-03-21 10:23:04 +0900

    🎉 :: project init

---
+++ SwiftUI-MVVM-Test/Sources/App/SwiftUI_MVVM_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_TestApp.swift
+//  SwiftUI-MVVM-Test
+//
+//  Created by Doyeon on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ SwiftUI-MVVM-Test/Sources/Views/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  MainView.swift
+//  SwiftUI-MVVM-Test
+//
+//  Created by Doyeon on 2023/03/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
