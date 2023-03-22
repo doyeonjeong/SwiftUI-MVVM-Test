fc049522f2236c749839bae56c5118057d42281d
doyeonjeong <debby_@kakao.com>
2023-03-22 17:26:45 +0900

    ➕ :: Add PersonModel

---
+++ SwiftUI-MVVM-Test/Sources/Models/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  SwiftUI-MVVM-Test
+//
+//  Created by Doyeon on 2023/03/22.
+//
+
+import SwiftUI
+
+struct Person {
+    var id = UUID()
+    var name: String
+    var email: String
+    var phone: String
+}
