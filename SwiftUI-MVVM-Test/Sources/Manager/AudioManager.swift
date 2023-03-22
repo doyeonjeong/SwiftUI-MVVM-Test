bf3018cca39ba9ccb01a65686aca9da72251e5c4
doyeonjeong <debby_@kakao.com>
2023-03-22 17:25:07 +0900

    ✨ :: Add startPlayer()

--- SwiftUI-MVVM-Test/Sources/Manager/AudioManager.swift
+++ SwiftUI-MVVM-Test/Sources/Manager/AudioManager.swift
@@ -18,4 +18,23 @@ final class AudioManager {
     private func getPathUrl(forResource: String, musicExtension: MusicExtension) -> URL? {
         Bundle.main.url(forResource: forResource, withExtension: musicExtension.rawValue) ?? nil
     }
+    
+    func startPlayer(track: String, volume: Float? = 1.0) {
+        guard let url = getPathUrl(forResource: track, musicExtension: .mp3),
+              let volume = volume else {
+            self.player = nil
+            print("resource not found \(track)")
+            return
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.volume = volume
+            player?.numberOfLoops -= 1
+            player?.play()
+        } catch {
+            print("fail to initialize player")
+        }
+    }
 }
